de0d0d296331e074679d7ab94caa234055681297
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-01-20 16:05:10 +0100

    ✨ Customizable swagger docs endpoint

--- src/modules/swagger/__tests/swagger.test.ts
+++ src/modules/swagger/__tests/swagger.test.ts
@@ -1,12 +1,12 @@
 import { StatusCodes } from 'http-status-codes'
 import request from 'supertest'
-import { swaggerModule } from '..'
+import { swaggerModule, SwaggerModuleProps } from '..'
 import { createApplication } from '../../../application'
 
-const createTestApp = () => createApplication({
+const createTestApp = (props?: SwaggerModuleProps) => createApplication({
 	openApiDefinitionPath: 'src/__tests/test-app.openapi.yml',
 	validateResponse: true,
-}).use(swaggerModule())
+}).use(swaggerModule(props))
 
 describe('swagger-module', () => {
 	it('GET /swagger.json responds with JSON', async () => createTestApp().run(
@@ -28,6 +28,31 @@ describe('swagger-module', () => {
 		async server => {
 			const { status, headers } = await request(server)
 				.get('/swagger')
+			expect(status).toBe(StatusCodes.OK)
+			expect(headers['content-type']).toMatch(/html/)
+		}))
+})
+
+describe('swagger-module, routePrefix=/api/v1/test', () => {
+	it('GET /api/v1/test/swagger.json responds with JSON', async () => createTestApp({ routePrefix: '/api/v1/test' }).run(
+		async server => {
+			const { headers, status } = await request(server)
+				.get('/api/v1/test.json')
+				.set('Accept', 'application/json')
+			expect(headers['content-type']).toMatch(/json/)
+			expect(status).toEqual(StatusCodes.OK)
+		}))
+	it('GET / redirects to /api/v1/test', async () => createTestApp({ routePrefix: '/api/v1/test' }).run(
+		async server => {
+			const { status, headers } = await request(server)
+				.get('/')
+			expect(status).toBe(StatusCodes.MOVED_TEMPORARILY)
+			expect(headers['location']).toBe('/api/v1/test')
+		}))
+	it('GET /api/v1/test/swagger responds with HTML', async () => createTestApp({ routePrefix: '/api/v1/test' }).run(
+		async server => {
+			const { status, headers } = await request(server)
+				.get('/api/v1/test')
 			expect(status).toBe(StatusCodes.OK)
 			expect(headers['content-type']).toMatch(/html/)
 		}))
